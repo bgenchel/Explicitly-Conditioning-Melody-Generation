b6ce549a30fd14598ec299ca4008ae8d9f20f3eb
bgenchel <benjiegenchel@gmail.com>
2018-10-16 22:45:58 -0400

    removed harmony_timing method, not needed

--- src/processing/conversion/xml_to_json.py
+++ src/processing/conversion/xml_to_json.py
@@ -22,7 +22,6 @@ def xml_to_dict(fpath):
                 child_dict = recurse(child, child_dict)
 
             if child.tag == "measure":
-                child_dict["harmonies_start"] = harmony_timing(child)
                 root_dict["measures"].append(child_dict)
             elif child.tag == "harmony":
                 root_dict["groups"].append({"harmony": child_dict, "notes": []})
@@ -42,29 +41,6 @@ def xml_to_dict(fpath):
     xml_dict[root.tag] = {"attributes": root.attrib}
     return recurse(root, xml_dict)
 
-# Used to get the start position of a chord for use in MIDI tick representation
-def harmony_timing(measure):
-    harmonies_start = []
-
-    position = 0
-
-    for el in measure:
-        # Advance position for note
-        if el.tag == "note":
-            for child in el:
-                if child.tag == "duration":
-                    position += int(child.text)
-        elif el.tag == "forward":
-            # Forward has one child, duration
-            position += int(el[0].text)
-        elif el.tag == "backward":
-            # Backward has one child, duration
-            position -= int(el[0].text)
-        elif el.tag == "harmony":
-            # Capture the start time of this harmony
-            harmonies_start.append(position)
-
-    return harmonies_start
 
 if __name__ == '__main__':
     root_dir = str(Path(op.abspath(__file__)).parents[3])
