932d3b169a545ab319aa93f7f4616d57a42989f4
bgenchel <benjiegenchel@gmail.com>
2019-02-06 15:09:08 -0500

    got chord_cond model working with embedding chord_root and encoding pitch classes, etc.

--- src/models/chord_cond/train_model.py
+++ src/models/chord_cond/train_model.py
@@ -11,7 +11,8 @@ sys.path.append(str(Path(op.abspath(__file__)).parents[2]))
 import utils.helpers as hlp
 from utils.training import Trainer
 
-torch.cuda.set_device(0)
+if torch.cuda.is_available():
+    torch.cuda.set_device(0)
 
 run_datetime_str = datetime.now().strftime('%b%d-%y_%H:%M:%S')
 args = hlp.get_args(default_title=run_datetime_str)
